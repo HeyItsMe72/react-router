ad9cb25248693f36ccc82e92f444d137e9a2fd00
Lucia Esteves <c.luciaest.72@gmail.com>
2024-03-19 20:59:28 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1>React Router</h1>
+      <a href="https://reactrouter.com/en/6.22.3/start/tutorial" rel='noreferrer' target='_blank'>
+        Documentation
+      </a>
+    </>
+  )
+}
+
+export default App
